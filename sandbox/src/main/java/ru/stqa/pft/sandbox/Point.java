5005bf87dcb193344e8bbeb693051f318b8c9278
Ekaterina Mezga <mezga@targetprocess.com>
2019-02-05 13:10:49 +0300

    Move distance(Point a, Point b) method to Task2 class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,17 +2,13 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-    private double x;
-    private double y;
+    public double x;
+    public double y;
 
     public Point(double x, double y){
 
         this.x = x;
         this.y = y;
-    }
-
-    public static double distance(Point a, Point b){
-        return Math.sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
     }
 
     public double distanceToPoint(Point point){
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -7,10 +7,14 @@ public class Task2 {
         Point pointA = new Point(2, 4);
         Point pointB = new Point(5, 8);
 
-        System.out.println("Distance between points is " + Point.distance(pointA, pointB));
+        System.out.println("Distance between points is " + distance(pointA, pointB));
 
         System.out.println("Distance from pointA point to pointB is " + pointA.distanceToPoint(pointB));
 
+    }
+
+    public static double distance(Point a, Point b){
+        return Math.sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
     }
 
 }
