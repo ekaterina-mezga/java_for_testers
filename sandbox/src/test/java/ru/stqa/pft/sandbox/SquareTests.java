a610e7c963021682509c80f1fee42e8f99c99a0f
Ekaterina Mezga <mezga@targetprocess.com>
2019-02-05 13:02:40 +0300

    Add SquareTests class

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+    @Test
+    public void testArea(){
+        Square square = new Square(5);
+        square.area();
+        Assert.assertEquals(square.area(), 25.0);
+    }
+}
