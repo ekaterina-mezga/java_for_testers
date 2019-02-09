acc4aca0c5f03e842dbe5feeee341765ce6a204d
Ekaterina Mezga <emezga@gmail.com>
2019-02-09 18:31:40 +0300

    Pull up members of ContactCreationTests class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -96,4 +96,34 @@ public class TestBase {
     protected void selectGroup() {
       driver.findElement(By.name("selected[]")).click();
     }
+
+    protected void returnToHomePage() {
+      driver.findElement(By.linkText("home page")).click();
+    }
+
+    protected void submitContactCreation() {
+      driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    protected void fillCreateContactForm(ContactData contactData) {
+      driver.findElement(By.name("firstname")).click();
+      driver.findElement(By.name("firstname")).clear();
+      driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+      driver.findElement(By.name("lastname")).click();
+      driver.findElement(By.name("lastname")).clear();
+      driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+      driver.findElement(By.name("address")).click();
+      driver.findElement(By.name("address")).clear();
+      driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
+      driver.findElement(By.name("home")).click();
+      driver.findElement(By.name("home")).clear();
+      driver.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
+      driver.findElement(By.name("email")).click();
+      driver.findElement(By.name("email")).clear();
+      driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    protected void initContactCreation() {
+      driver.findElement(By.linkText("add new")).click();
+    }
 }
