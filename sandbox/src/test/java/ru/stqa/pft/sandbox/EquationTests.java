0eb12d3202f7e676e2c71a154453fa5101c1c29c
Ekaterina Mezga <emezga@gmail.com>
2019-02-17 17:05:56 +0300

    Refactoring Equation.java, add more tests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -6,20 +6,38 @@ import org.testng.annotations.Test;
 public class EquationTests {
 
   @Test
-  public void TestZeroRoots(){
+  public void testZeroRoots(){
     Equation e = new Equation(1, 1, 1);
     Assert.assertEquals(e.rootNumber(), 0);
   }
 
   @Test
-  public void TestOneRoot(){
+  public void testOneRoot(){
     Equation e = new Equation(1, 2, 1);
     Assert.assertEquals(e.rootNumber(), 1);
   }
 
   @Test
-  public void TestTwoRoots(){
+  public void testTwoRoots(){
     Equation e = new Equation(1, 5, 6);
     Assert.assertEquals(e.rootNumber(), 2);
+  }
+
+  @Test
+  public void testLinear(){
+    Equation e = new Equation(0, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void testConstant(){
+    Equation e = new Equation(0, 0, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void testZeroEqualsZero(){
+    Equation e = new Equation(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(), -1);
   }
 }
