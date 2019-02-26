a57e6705c7177abfe3f946acc6d3d4631633fc8b
Ekaterina Mezga <mezga@targetprocess.com>
2019-02-26 12:59:20 +0300

    Add HashSet to GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -80,7 +80,7 @@ public class GroupHelper extends BaseHelper {
     List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
     for (WebElement e: elements){
       String name = e.getText();
-      String id = e.findElement(By.tagName("input")).getAttribute("value");
+      int id = Integer.parseInt(e.findElement(By.tagName("input")).getAttribute("value"));
       GroupData group = new GroupData(id, name, null, null);
       groups.add(group);
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -6,9 +6,9 @@ public class GroupData {
   private final String name;
   private final String header;
   private final String footer;
-  private final String id;
+  private int id;
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
@@ -16,36 +16,16 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name) &&
-            Objects.equals(id, groupData.id);
-  }
+  public int getId() { return id; }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, id);
-  }
-
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            ", id='" + id + '\'' +
-            '}';
-  }
-
-  public String getId() {
-    return id;
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -60,4 +40,25 @@ public class GroupData {
     return footer;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id &&
+            Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, id);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
+  }
 }
