526d0efff65a832ebcbd431c66e3afd8b0e1dc69
Ekaterina Mezga <mezga@targetprocess.com>
2019-03-04 16:10:44 +0300

    Modification of contacts creation/deletion and groups creation/deletion tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -12,15 +12,14 @@ public class ContactCreationTests extends TestBase{
   @Test
   public void testContactCreation(){
     app.goTo().homePage();
-   Contacts before = app.contact().all();
+    Contacts before = app.contact().all();
     ContactData contact = new ContactData()
             .withFirstName("Sherlock").withLastName("Holmes")
             .withAddress("London, Backer str., 221b").withHomePhone("222 22 22").withEmail("sherlock@test.test").withGroup("group1");
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
-
-   assertThat(after, equalTo(
+    assertThat(after, equalTo(
            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 }
