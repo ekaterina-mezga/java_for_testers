bab37e75c22cd785b27529daa630b7198046d6b4
Ekaterina Mezga <emezga@gmail.com>
2019-03-12 18:25:16 +0300

    Added support of data generation in json format

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -7,11 +8,15 @@ import java.util.Objects;
 
 @XStreamAlias("group")
 public class GroupData {
-  private String name;
-  private String header;
-  private String footer;
   @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+  @Expose
+  private String name;
+  @Expose
+  private String header;
+  @Expose
+  private String footer;
+
 
   public int getId() { return id; }
 
