f4b666b6aafb7060e59a98de1a01e1f29107d45c
Ekaterina Mezga <emezga@gmail.com>
2019-03-17 13:37:58 +0300

    Using hibernate to obtain contacts list

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,47 +3,89 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
-  @Expose
-  private String firstName;
-  @Expose
-  private String lastName;
-  @Expose
-  private String address;
-  @Expose
-  private String homePhone;
-  @Expose
-  private String email;
-  @Expose
-  private String email2;
-  @Expose
-  private String email3;
-  @Expose
-  private String allEmails;
+
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+
   @Expose
-  private String group;
+  @Column(name = "firstname")
+  private String firstName;
+
   @Expose
+  @Column(name = "lastname")
+  private String lastName;
+
+  @Expose
+  @Column(name = "address")
+  @Type(type = "text")
+  private String address;
+
+  @Expose
+  @Column(name = "home")
+  @Type(type = "text")
+  private String homePhone;
+
+  @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+
   @Expose
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
 
+  @Transient
   private String allPhones;
 
-  private File photo;
+  @Expose
+  @Column(name = "email")
+  @Type(type = "text")
+  private String email;
+
+  @Expose
+  @Column(name = "email2")
+  @Type(type = "text")
+  private String email2;
+
+  @Expose
+  @Column(name = "email3")
+  @Type(type = "text")
+  private String email3;
+
+  @Expose
+  @Transient
+  private String allEmails;
+
+  @Expose
+  @Transient
+  private String group;
+
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public File getPhoto() {
-    return photo;
+    if (photo == null){
+      return null;
+    } else
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
