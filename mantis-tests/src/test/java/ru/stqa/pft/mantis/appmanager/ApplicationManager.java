315c3027ba22642387f541a8a843ccb5a99ee607
Ekaterina Mezga <emezga@gmail.com>
2019-03-23 19:35:36 +0300

    Add HttpSession class

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -43,4 +43,12 @@ public class ApplicationManager {
   public void stop() {
     driver.quit();
   }
+
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
