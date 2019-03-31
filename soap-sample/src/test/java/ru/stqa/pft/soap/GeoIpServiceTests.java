2b9a1360fe59ee7ccebf849b6d353e7c42479228
Ekaterina Mezga <47274654+ekaterina-mezga@users.noreply.github.com>
2019-03-31 13:40:41 +0300

    GeoIpService example

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp(){
+
+    String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("46.53.252.52");
+    Assert.assertTrue(ipLocation.contains("<Country>BY</Country>"));
+  }
+}
