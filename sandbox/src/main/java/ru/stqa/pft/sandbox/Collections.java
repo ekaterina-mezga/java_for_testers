3d91f9b5add321e22d64b45fc9e066b9d76661b8
Ekaterina Mezga <mezga@targetprocess.com>
2019-04-12 10:59:14 +0300

    Add another example with array to sandbox

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -64,5 +64,32 @@ public class Collections {
 
     System.out.println(" ");
 
+    // Move all zeros to the end
+    int[] a = {1, 2, 0, 0, 3, 0, 4, 0, 5, 6, 0, 0, 0, 7, 0, 0, 8, 9, 0, 10};
+
+    for (int num : a){
+      System.out.print(num + " ");
+    }
+
+    System.out.println(" ");
+
+    int index = 0;
+    int limit = a.length;
+
+    while (index < limit){
+      if (a[index] == 0){
+        for (int j = index; j<limit-1; j++){
+          a[j] = a[j+1];
+        }
+        limit--;
+        a[limit] = 0;
+      } else index++;
+    }
+
+    for (int num : a){
+      System.out.print(num + " ");
+    }
+
+    System.out.println(" ");
   }
 }
